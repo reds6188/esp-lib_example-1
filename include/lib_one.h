c497cf971d5798e72b585779587d37410e3915a8
Daniele Rossi <73635936+reds6188@users.noreply.github.com>
2024-12-19 11:31:22 +0100

    First version

---
+++ include/lib_one.h
@@ -0,0 +1,8 @@
+#ifndef LIB_ONE_H_
+#define LIB_ONE_H_
+
+#include <Arduino.h>
+
+void funcOne(void);
+
+#endif  /* LIB_ONE_H_ */
---
+++ src/lib_one.cpp
@@ -0,0 +1,5 @@
+#include "lib_one.h"
+
+void funcOne(void) {
+    Serial.println("Library ONE");
+}
